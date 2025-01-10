93d89604234c177433e8a105e9b74797536b26e2
ArtemBielkovskyi <157801683+ArtemBielkovskyi@users.noreply.github.com>
2025-01-10 11:31:05 +0000

    Deleted excessive bits of code

--- html/AccountInfo.php
+++ html/AccountInfo.php
@@ -46,22 +46,6 @@
         <link rel="stylesheet" type="text/css" href="AccountInfo.css">
     </head>
     <body>
-        <!-- <div class="header">
-            <div class="logo", onclick="window.open('../Index.php','_Self')">K1J LTD</div>
-            <a href="../Index.php"><button><i class="fas fa-house"></i>Home</button></a>
-            <a href="UnSignedAnalyticsPage.php"><button><i class="fa-solid fa-chart-line"></i>Analytics</button></a>
-            <a href="LoginPage.php"><button class="Log"><i class="fa-solid fa-right-to-bracket"></i>Login</button></a>
-            <a href="Registration.php"><button class="Reg"><i class="fa-solid fa-address-card"></i>Register</button></a>
-            <?php if (isset($_SESSION['email'])): ?>
-				<script>
-                    document.querySelector('.Log').style.display = 'none';
-                    document.querySelector('.Reg').style.display = 'none';
-                </script>
-                <div class="usericon" onclick='window.open("AccountInfo.php","_self")'><?php echo $_SESSION['username'];?></div>
-                <form action="logout.php" method="POST"><button class="logoff">Log off</button></form>
-            <?php endif; ?>
-        </div> -->
-
         <div class="AccountInforamtion">
             <div class="MainInformation">
                 <span class="AccountInfo">Account Information</span>
